0275daf7b706e7dea4eca3e2fddc0a4286775379
Pamindu Nawodya <pamidunawodya@gmail.com>
2024-01-19 22:45:29 +0530

    add controller,password bcrypt

---
+++ src/controllers/article.controllers.ts
@@ -0,0 +1,220 @@
+
+//create article Route
+import express from "express";
+import ArticleModel from "../models/article.model";
+import {ObjectId} from "mongodb";
+import customResponse from "../dtos/custom.response";
+import UserModel from "../models/user.model";
+import * as Middleware from "../middlewares";
+
+export const createArticle=async (req:express.Request, res:express.Response)=>{
+    try {
+
+        let req_body=req.body;
+
+        console.log(req_body);
+
+        const articleModel= new ArticleModel({
+            title:req_body.title,
+            description:req_body.description,
+            user:new ObjectId(req_body.user)
+        });
+
+        await articleModel.save().then(r=> {
+            res.status(200).send(
+                new customResponse(200,"Article Created Sucessfully")
+            )
+
+        }).catch(e=>{
+            res.status(100).send(
+                new customResponse(100,"Something Went Wrong")
+            )
+            console.log(e);
+
+        });
+
+
+
+    }catch (error){
+        res.status(401).send("error");
+    }
+
+}
+
+//get all articles Route
+export const  getAllArticles=async (req:express.Request,res:express.Response)=> {
+
+    try {
+
+        let req_query:any=req.query;
+        let size:number=req_query.size;
+        let page:number=req_query.page;
+
+
+
+        //size 10 page 1 nam val:(db eke collection wala tiyana 1*10 =10 palaweni 10 ain karl ithuru tike 1 weni 10 gnnw)
+        //const articles = await ArticleModel.find().limit(req.query.size).skip(req.query.size*req.page);
+        //error resolving
+        const articles = await ArticleModel.find().limit(size).skip(size*(page-1));
+
+
+        let documentCount=await ArticleModel.countDocuments();
+        let pageCount=Math.ceil(documentCount/size);
+
+
+        res.status(200).send(
+            new customResponse(200, "Articles Are Found Sucessfully", articles,pageCount)
+        );
+
+    }catch (error){
+        res.status(100).send("Error")
+    }
+
+}
+
+//get  articles By username
+export const getArticleByUserName=async (req:express.Request,res:express.Response)=>{
+
+    try {
+
+        let req_query:any=req.query;
+        let size:number=req_query.size;
+        let page:number=req_query.page;
+
+
+        // console.log(req.params.username);
+
+        let username:string=req.params.username;
+
+        let user= await UserModel.findOne({username:username});
+
+        //user kenek nttm
+        if (!user){
+            res.status(404).send(
+                new  customResponse(404,"user are not found")
+            )
+        }else {
+            let articles=await ArticleModel.find({user:user._id}).limit(size).skip(size*(page-1));
+
+            let documentCount=await ArticleModel.countDocuments({user:user._id});
+            let pageCount=Math.ceil(documentCount/size);
+
+
+            res.status(200).send(
+                new customResponse(200,"Articles Are Found Sucessfully",articles,pageCount)
+            )
+        }
+    }catch (error){
+        res.status(100).send("Error")
+    }
+}
+
+//get my articles(Token)
+export const getMyArticle=async (req:express.Request,res:any)=>{
+
+    try {
+
+        let req_query:any=req.query;
+        let size:number=req_query.size;
+        let page:number=req_query.page;
+
+
+        let user_id=res.tokenData.user._id;
+
+        let articles=await ArticleModel.find({user:user_id}).limit(size).skip(size*(page-1));
+
+        let documentCount=await ArticleModel.countDocuments({user:user_id});
+
+        let pageCount=Math.ceil(documentCount/size);
+
+        res.status(200).send(
+            new customResponse(200,'Articles Are Found Sucessfully',articles,pageCount)
+        )
+
+
+
+    }catch (error){
+        res.status(100).send("Not Found")
+    }
+
+}
+
+//update my articles(Token)
+export const updateArticle=async (req:express.Request,res:any)=>{
+
+
+    try{
+
+        //check article by user
+
+        let req_body:any=req.body
+        let user_id=res.tokenData.user._id;
+        let article=  await ArticleModel.find({_id:req_body.id,user:user_id})
+
+        console.log('test');
+
+        if (article){
+
+            await ArticleModel.findByIdAndUpdate({_id:req_body.id},{
+                title:req_body.title,
+                description:req_body.description
+            }).then(r=>{
+                res.status(200).send(
+                    new customResponse(200,"Article Updated Sucessfully")
+                )
+            }).catch(e=>{
+                console.log(e);
+                res.status(100).send(
+                    new customResponse(100,"Something went Wrong")
+                )
+            })
+
+        }else {
+            res.status(401).send(
+                new customResponse(401,"Access Denied")
+            )
+        }
+
+
+    }catch (error){
+        res.status(100).send(error);
+    }
+}
+
+//delete my articles(Token)
+export const deleteArticle=async (req:express.Request,res:any)=>{
+
+
+    try{
+
+        let articleID:string=req.params.id;
+        let user_id=res.tokenData.user._id;
+        let article=  await ArticleModel.find({_id:articleID,user:user_id})
+
+        if (article){
+
+            await ArticleModel.deleteOne({_id:articleID,user:user_id}).then(r=>{
+
+                res.status(200).send(
+                    new customResponse(200,"Article Deleted Sucessfully")
+                )
+            }).catch(e=>{
+                console.log(e);
+                res.status(100).send(
+                    new customResponse(100,"Something went Wrong")
+                )
+            })
+
+
+        }else {
+            res.status(401).send(
+                new customResponse(401,"access denied")
+            )}
+
+
+    }catch (error){
+        res.status(100).send(error);
+    }
+
+
+}
---
+++ src/controllers/user.controller.ts
@@ -0,0 +1,141 @@
+//get all user
+import express from "express";
+import ArticleModel from "../models/article.model";
+import {ObjectId} from "mongodb";
+import customResponse from "../dtos/custom.response";
+import UserModel from "../models/user.model";
+import * as SchemaTypes from "../types/SchemaTypes";
+import jwt, {Secret} from "jsonwebtoken";
+import process from "node:process";
+import bcrypt from "bcryptjs";
+
+//middle wares controllers athulata gennh
+
+//all users
+export const getAllUser=async (req:express.Request, res:any)=>{
+    // res.send("hello");
+
+    // let data={
+    //     name:"kamal",
+    //     username:"Saman,",
+    //     age:22,
+    //     email:"kamal@ijse.com"
+    //
+    // }
+    // res.send(data);
+
+    //DB eke gson data tika json widiyta gnnaw
+    try {
+        let users=  await UserModel.find();
+        res.status(200).send(
+            new customResponse(200,"User are found Sucessfully",users)
+        );
+        //res.send(users);
+    }catch (error){
+        res.status(100).send("!Error");
+    }
+
+}
+
+
+//create new user
+export const newUser=async (req:express.Request,res:express.Response)=>{
+
+    try {
+        const  req_body:any=req.body;
+        console.log(req_body);
+
+        //encript password
+        await bcrypt.hash(req_body.password,8,async function (err, hash) {
+            if (err) {
+                res.status(100).send(
+                    new customResponse(200, "Something went wrong")
+                )
+
+            }
+            const userModel = new UserModel({
+                username: req_body.username,
+                fname: req_body.fname,
+                lname: req_body.lname,
+                email: req_body.email,
+                password:hash
+            })
+            // let user:Iuser|null=await  userModel.save();
+            let user: SchemaTypes.Iuser | null = await userModel.save();
+
+
+            if (user) {
+                user.password = "";
+                res.status(200).send(
+                    new customResponse(200, "User Created Sucessfully", user)
+                );
+            }else {
+                res.status(100).send(
+                    new customResponse(200, "Something went wrong")
+                )
+            }
+        })
+
+        //db eken  value tika set krnwk
+
+
+
+        // users.push(req_body)
+    }catch (error){
+        res.status(100).send("Error")
+    }
+
+
+
+
+    // res.send( "okay");
+}
+
+//auth
+export const signUsers=async (req:express.Request,res:express.Response)=>  {
+
+
+
+    let request_body=req.body
+    //email , password
+
+    let user:SchemaTypes.Iuser|null=await UserModel.findOne({email:request_body.email});
+    if(user){
+        if (user.password==request_body.password){
+
+            //0
+            user.password="";
+
+            const expiresIn='1w'
+
+            jwt.sign({user},process.env.SECRET as Secret,{expiresIn},(err:any,token:any )=>{
+                if(err){
+                    res.status(100).send(
+                        new customResponse(100,"Something Went Wrong")
+                    );
+                } else {
+
+                    let  res_body={
+                        user:user,
+                        accessToken:token
+                    }
+
+                    res.status(200).send(
+                        new customResponse(200,"Access",res_body)
+                    );
+                }
+            })
+
+        }else {
+            //user innw password invalid
+            res.status(401).send(
+                new customResponse(401,"Invalid creditials")
+            )
+        }
+    }else {
+        res.status(404).send(
+            new customResponse(404,"user not found")
+        );
+
+    }
+}
